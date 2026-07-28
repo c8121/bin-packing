ba5ccf7495cf5d4b9df76a62c4e837edba66a72e
Christian Hau <ch.hau@gefro.de>
2026-07-28 15:32:49 +0200

    Three.js: Fix CDN-Integration. User can select & highlight cube

--- BinPacking/src/main/java/de/c8121/packing/visualization/ThreeJs.java
+++ BinPacking/src/main/java/de/c8121/packing/visualization/ThreeJs.java
@@ -69,76 +69,124 @@ public class ThreeJs {
         var html = new StringBuilder();
 
         html
-                .append("<!DOCTYPE html>\n")
-                .append("<html>\n")
-                .append("<head>\n")
-                .append("<meta charset=\"utf-8\" />\n")
-                .append("</head>\n")
-                .append("<body>\n")
-                .append("<script src=\"https://threejs.org/build/three.js\"></script>\n")
-                .append("<script src=\"https://threejs.org/examples/js/controls/OrbitControls.js\"></script>\n")
-                .append("<script>\n");
-
-        html
-
-                .append("const scene = new THREE.Scene();\n")
-                .append("const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, -500000, 0);\n")
-                .append("camera.position.z = 55000;\n")
-                .append("camera.position.y = 1;\n")
-                .append("camera.position.z = 1;\n")
-                .append("camera.up.set( 0, 0, 1 );\n")
-                .append("const renderer = new THREE.WebGLRenderer();\n")
-                .append("renderer.setSize( window.innerWidth, window.innerHeight );\n")
-                .append("renderer.setClearColor( 0xeeeeee );\n")
-                .append("document.body.appendChild( renderer.domElement );\n");
-
-        html
-                .append("let material;\n")
-                .append("let geometry;\n")
-                .append("let cube;\n");
+                .append("""
+                        <!DOCTYPE html>
+                        <html>
+                        <head>
+                        <meta charset="utf-8" />
+                        </head>
+                        <body>
+                        
+                        <select id="cubeSelect">
+                            <option>Select</option>
+                        </select>
+                        
+                        <script type="importmap">
+                        {
+                            "imports": {
+                                "three": "https://cdn.jsdelivr.net/npm/three@v0.185.1/build/three.module.js",
+                                "three/examples/jsm/controls/OrbitControls": "https://cdn.jsdelivr.net/npm/three@0.185.1/examples/jsm/controls/OrbitControls.js",
+                                "three/examples/jsm/libs/stats.module": "https://cdn.jsdelivr.net/npm/three@0.185.1/examples/jsm/libs/stats.module.js"
+                            }
+                        }
+                        </script>
+                        
+                        <script type="module">
+                        
+                        import * as THREE from 'three';
+                        import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+                        import Stats from 'three/examples/jsm/libs/stats.module';
+                        
+                        const scene = new THREE.Scene();
+                        
+                        const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, -500000, 0);
+                        camera.position.z = 55000;
+                        camera.position.y = 1;
+                        camera.position.z = 1;
+                        camera.up.set( 0, 0, 1 );
+                        
+                        const renderer = new THREE.WebGLRenderer();
+                        renderer.setSize( window.innerWidth, window.innerHeight );
+                        renderer.setClearColor( 0xeeeeee );
+                        document.body.appendChild( renderer.domElement );
+                        
+                        const cubes = [];
+                        const cubeSelect = document.getElementById("cubeSelect");
+                        
+                        function addCube(x, y, z, xs, ys, zs, style, selectable) {
+                           const geometry = new THREE.BoxGeometry(xs, ys, zs);
+                           const material = new THREE.MeshBasicMaterial(style);
+                           const cube = new THREE.Mesh(geometry, material);
+                           cube.position.x = x;
+                           cube.position.y = y;
+                           cube.position.z = z;
+                        
+                           scene.add(cube);
+                        
+                           if(selectable) {
+                                cubes.push(cube);
+                                cubeSelect.append(new Option(cubes.length));
+                           }
+                        
+                           return cube;
+                        }
+                        
+                        """);
 
         for (var box : this.boxes) {
-
-            html.append("geometry = new THREE.BoxGeometry(")
-                    .append(box.xs()).append(", ")
-                    .append(box.ys()).append(",")
-                    .append(box.zs())
-                    .append(");\n");
 
             var style = this.styles.get(box);
             if (style == null)
                 style = "color: 0x000000, wireframe: true";
 
-            html.append("material = new THREE.MeshBasicMaterial({").append(style).append("});\n");
-
-            html.append("cube = new THREE.Mesh(geometry, material);\n")
-                    .append("cube.position.x = ").append(box.x()).append(";\n")
-                    .append("cube.position.y = ").append(box.y()).append(";\n")
-                    .append("cube.position.z = ").append(box.z()).append(";\n");
-
-            html.append("scene.add(cube)\n");
+            html.append("addCube(")
+                    .append(box.x()).append(", ")
+                    .append(box.y()).append(", ")
+                    .append(box.z()).append(", ")
+                    .append(box.xs()).append(", ")
+                    .append(box.ys()).append(", ")
+                    .append(box.zs()).append(", ")
+                    .append("{").append(style).append("}").append(", ")
+                    .append("true")
+                    .append(");\n");
         }
 
-        html
-                .append("camera.position.x = 500;\n")
-                .append("camera.position.y = 500;\n")
-                .append("camera.position.z = 500;\n");
 
-        html
-                .append("function animate() {\n")
-                .append("renderer.render( scene, camera );\n")
-                .append("}\n");
+        html.append("""
+                camera.position.x = 500;
+                camera.position.y = 500;
+                camera.position.z = 500;
+                
+                let highlightCube = null;
+                
+                cubeSelect.addEventListener("change", (e) => {
+                    const selectedIdx = e.target.value;
+                    if(isNaN(selectedIdx))
+                        return;
+                
+                    const cube = cubes[selectedIdx-1];
+                    const size = cube.geometry.parameters;
+                    const pos = cube.position;
+                
+                    if( highlightCube != null ) {
+                        scene.remove(highlightCube);
+                    }
+                    highlightCube = addCube(pos.x, pos.y, pos.z, size.width, size.height, size.depth, {color: 'green', wireframe: true}, false);   \s
+                
+                    animate();
+                });
 
-        html
-                .append("var controls = new THREE.OrbitControls( camera, renderer.domElement );\n")
-                .append("controls.addEventListener( 'change', animate );\n")
-                .append("animate();\n");
-
-        html
-                .append("</script>\n")
-                .append("</body>\n")
-                .append("</html>\n");
-
+                function animate() {
+                    renderer.render( scene, camera );
+                }
+                
+                var controls = new OrbitControls( camera, renderer.domElement );
+                controls.addEventListener( 'change', animate );
+                animate();
+                </script>
+                </body>
+                </html>
+                """);
 
         return html.toString();
     }
